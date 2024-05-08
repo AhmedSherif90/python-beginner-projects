0fc4fdbaa9b590b1a9be125d6089597456ee6de0
AhmedSherif90 <57321466+AhmedSherif90@users.noreply.github.com>
2024-05-08 03:39:03 +0300

    Delete Day-1-Printing directory

--- Day-1-Printing/Main.py
+++
@@ -1,9 +0,0 @@
-#1. Create a greeting for your program.
-print("Welcome to the band name generator")
-#2. Ask the user for the city that they grew up in.
-City_name = input("What is your city name?\n ")
-#3. Ask the user for the name of a pet.
-Pet_name = input("What is your Pet name?\n  ")
-#4. Combine the name of their city and pet and show them their band name.
-print("your band name should be "+City_name+" "+Pet_name)
-
