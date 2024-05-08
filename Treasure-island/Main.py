319f4b36bf300ffef01fe7d615fb3cea27b8d9ee
AhmedSherif90 <57321466+AhmedSherif90@users.noreply.github.com>
2024-05-08 04:21:11 +0300

    Add files via upload

---
+++ Treasure-island/Main.py
@@ -0,0 +1,6 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Round the result to 2 decimal places.
+bill=5
+print("Welcome to the tip calculator!")
+print(f"Your final bill is ${bill}")
