74bc0dbec05699e6daa1a96dcc9ba837ae924adb
AhmedSherif90 <57321466+AhmedSherif90@users.noreply.github.com>
2024-05-10 16:53:27 +0300

    Update Main.py

--- Rock Paper Scissors/Main.py
+++ Rock Paper Scissors/Main.py
@@ -1,6 +1,5 @@
 import random
 
-
 rock = '''
     _______
 ---'   ____)
@@ -27,48 +26,19 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_images = [rock, paper, scissors]
+choice = (int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n0/1/2:")))
+computers_choice = random.randint(0, 2)
 
-
-choice = (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n0/1/2:"))
-computers_choice = random.randint(0,2)
-
-if not choice.isdigit():
-  print("You've entered an invalid value, try again and choose a number between 0-2.")
+if choice < 0 or choice > 2:
+	print("You've entered an invalid value, try again and choose a number between 0-2.")
 else:
-  choice = int(choice)
-  if choice > 2:
-    print("You've entered an invalid number, try again and choose a number between 0-2.")
-  elif choice == 0 or choice == 1 or choice == 2:
-    if choice == 0:
-      print(f"You chose: Rock {rock}")
-    elif choice == 1:
-      print(f"You chose: Paper {paper}")
-    elif choice == 2:
-      print(f"You chose: Scissors {scissors}")
-
-    if computers_choice == 0:
-      print(f"The computer chose: Rock {rock}")
-      if choice == 2:
-        print("You lose, Rock wins against scissors.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
-
-    elif computers_choice == 1:
-      print(f"The computer chose: Paper {paper}")
-      if choice == 0:
-        print("You lose, Paper wins against rock.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
-
-    elif computers_choice == 2:
-      print(f"The computer chose: Scissors {scissors}")
-      if choice == 1:
-        print("You lose, Scissors win against paper.")
-      elif choice == 2:
-        print("It's a draw!")
-      else:
-        print("You win!")
+	print(game_images[choice])
+	print("Computer chose:")
+	print(game_images[computers_choice])
+	if choice == computers_choice:
+		print("It's a draw!")
+	elif choice == 0 and computers_choice == 1 or (choice == 1 and computers_choice == 2):
+		print("Computer wins!")
+	else:
+		print("You wins!")
